4e701c6cae220fa85a541c3febb94e5f126547cd
Sone <sthaya@gmail.com>
2020-11-17 18:35:12 -0800

    item screen displays the item that was selected on the homescreen

--- src/screens/ItemScreen.js
+++ src/screens/ItemScreen.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup } from "react-bootstrap";
+import Rating from "../components/Rating";
+import items from "../items";
 
-const ItemScreen = () => {
-    return (
-        <div>
-            Item
-        </div>
-    )
-}
+const ItemScreen = ({ match }) => {
+  const item = items.find((i) => i.manifestId === Number(match.params.id));
+  console.log(item);
 
-export default ItemScreen
+  return (
+    <>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image
+            src={item.imageUrl}
+            alt={item.name}
+            className="my-3 p-3 rounded bg-info"
+            fluid
+          />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{item.name}</h3>
+            </ListGroup.Item>
+            
+            <ListGroup.Item>{item.vBucks} vBucks</ListGroup.Item>
+            <ListGroup.Item>Category: {item.storeCategory}</ListGroup.Item>
+            <ListGroup.Item>Rarity: {item.rarity}</ListGroup.Item>
+            <ListGroup.Item>
+              <Rating value={item.rating} text={`${item.numReviews} reviews`} />
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default ItemScreen;
