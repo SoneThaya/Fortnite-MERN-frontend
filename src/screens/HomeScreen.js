3ff7ea8989a3592008be5b4100ccf2cc2b7a8649
Sone <sthaya@gmail.com>
2020-11-18 21:00:03 -0800

    set up homescreen for getting data from store

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,31 +1,35 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Row, Col } from "react-bootstrap";
 import ShopItem from "../components/ShopItem";
+import { listItems } from "../actions/itemActions";
 
 const HomeScreen = () => {
-  const [items, setItems] = useState([]);
+  const dispatch = useDispatch();
+
+  const itemList = useSelector((state) => state.itemList);
+  const { loading, error, items } = itemList;
 
   useEffect(() => {
-    const fetchItems = async () => {
-      const { data } = await axios.get("/api/items");
-
-      setItems(data);
-    };
-
-    fetchItems();
-  }, []);
+    dispatch(listItems());
+  }, [dispatch]);
 
   return (
     <>
-      <h1>Shop Items</h1>
-      <Row>
-        {items.map((item) => (
-          <Col key={item.manifestId} sm={12} md={6} lg={4}>
-            <ShopItem item={item} />
-          </Col>
-        ))}
-      </Row>
+      <h1>Fortnite Items</h1>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {items.map((item) => (
+            <Col key={item.manifestId} sm={12} md={6} lg={4}>
+              <ShopItem item={item} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
