1a3c5cbe9270d655bb51ad30bf3a1cc276e72e9a
Sone <sthaya@gmail.com>
2020-11-18 20:43:37 -0800

    created item reducer and constants files

--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { itemListReducer } from "./reducers/itemReducers";
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  itemList: itemListReducer,
+});
 
 const initialState = {};
 
