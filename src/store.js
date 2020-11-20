ea4e82234179a036018cafef24504ce7e84101f1
Sone <sthaya@gmail.com>
2020-11-19 18:12:57 -0800

    create user login action, reducer and constants to setup user login

--- src/store.js
+++ src/store.js
@@ -2,13 +2,21 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { itemListReducer, itemDetailsReducer } from "./reducers/itemReducers";
+import { userLoginReducer } from "./reducers/userReducers";
 
 const reducer = combineReducers({
   itemList: itemListReducer,
   itemDetails: itemDetailsReducer,
+  userLogin: userLoginReducer,
 });
 
-const initialState = {};
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
+const initialState = {
+  userLogin: { userInfo: userInfoFromStorage },
+};
 
 const middleware = [thunk];
 
